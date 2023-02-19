11791b9a216ac64337e18ab8b3321acb07010a81
¨Joaquin <joaquinabadia04@gmail.com>
2023-02-19 18:45:53 -0300

    Desafio CLase 3

--- Desafio-clase-3.js
+++ Desafio-clase-3.js
@@ -1,24 +1,41 @@
 class ProductManager {
-    constructor(tittle, description,price,tumbnail,code,stock, id){
+    constructor(tittle, description,price,thumbnail,code,stock, id){
         this.products = []
         this.tittle = tittle
         this.description = description
         this.price = price
-        this.tumbnail = thumbnail
+        this.thumbnail = thumbnail
         this.code = code
         this.stock = stock
         this.id = id
     }
     addProduct(product){
+        
         if(this.tittle == '' || this.description== '' || this.price== '' || this.tumbnail =='' || this.code== '' || this.stock== '' || this.id== ''){
             return ('Todos los campos son requeridos')
-        }else if(this.code === this.id) {
-            return ('ya esta agregado')
         }
-        product.id = math.random().toString(16)
+        
         this.products.push(product)
     }
     getProduct(product){
         return this.products
     }
+    randomId(){
+        Math.random()
+    }
+    getProductById(id){
+        for (const elements of this.products) {
+            if(elements.id === id) {
+                return elements.tittle
+            }else{
+                return 'no se encontro'
+            }
+        }
+    }
 }
+
+const productManager = new ProductManager();
+
+console.log(productManager.addProduct({tittle:'hola', description:'producto',price:9999,tumbnail:'https://img.png',code:09876657,stock:28}))
+console.log(productManager.addProduct({tittle:'hola', description:'producto',price:9999,tumbnail:'https://img.png',code:09876657,stock:28}))
+console.log(productManager.getProduct())    
